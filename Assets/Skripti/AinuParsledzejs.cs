5e1427636ec73fb7bf38e33c71064a6b90089cca
Hdstr727 <helldestroerr05@gmail.com>
2023-06-06 09:20:31 +0300

    Izveidota iespeja parslegties starp ainām

--- Assets/Skripti/AinuParsledzejs.cs
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AinuParsledzejs : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void Sakt()
+	{
+		SceneManager.LoadScene("PilsetasKarte");
+	}
+
+	public void Atpakaļ()
+	{
+		SceneManager.LoadScene("Sakums");
+	}
+
+	public void Apturet()
+	{
+		Application.Quit();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
