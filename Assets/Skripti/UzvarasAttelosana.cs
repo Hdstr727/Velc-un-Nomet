a485e4ef96bb9da621cd27b4bc3fba5272d9df9a
Hdstr727 <helldestroerr05@gmail.com>
2023-06-08 17:23:13 +0300

    Izmainits skripta nosaukums, realizeta laika skaitīšana un ta izvadišana konsole ar aizkavi

---
+++ Assets/Skripti/UzvarasAttelosana.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UzvarasAttelosana : MonoBehaviour
+{
+    public GameObject uzvarasPanelis;
+    public GameObject zvaignze1;
+    public GameObject zvaignze2;
+    public GameObject zvaignze3;
+    private float startTime;
+    public float logDelay = 2f;
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    private void StartTimer()
+    {
+        startTime = Time.time;
+        StartCoroutine(LogElapsedTime());
+    }
+
+    private System.Collections.IEnumerator LogElapsedTime()
+    {
+        while (true)
+        {
+            float elapsedTime = Time.time - startTime;
+
+            // Parāda laiku
+            Debug.Log("Elapsed Time: " + elapsedTime.ToString("F2"));
+
+            // Gaida
+            yield return new WaitForSeconds(logDelay);
+        }
+    }
+    public void AttelotUzvaru()
+    {
+
+    }
+
+
+}
