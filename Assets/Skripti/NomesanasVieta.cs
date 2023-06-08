a49ad729c83f137711ef424cb405737db21886ce
Hdstr727 <helldestroerr05@gmail.com>
2023-06-08 18:17:46 +0300

    Realizeta zvaigžņu attēlošana atbilstoši laikam

--- Assets/Skripti/NomesanasVieta.cs
+++ Assets/Skripti/NomesanasVieta.cs
@@ -3,86 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class NomesanasVieta : MonoBehaviour, 
-	IDropHandler{
-	private float vietasZRot, velkObjZRot, rotacijasStarpiba;
-	private Vector2 vietasIzm, velkObjIzm;
-	private float xIzmStarpiba, yIzmStarpiba;
-	public Objekti objektuSkripts;
+public class NomesanasVieta : MonoBehaviour,
+    IDropHandler
+{
+    private float vietasZRot, velkObjZRot, rotacijasStarpiba;
+    private Vector2 vietasIzm, velkObjIzm;
+    private float xIzmStarpiba, yIzmStarpiba;
+    public Objekti objektuSkripts;
     //Definets mainigais lai parbaudit kad visas 12 mašinas ir savā vieta
     //Šo mainigo inkrementē katru reizi, kad mašina pielimējas pie savas vietas
     private static int masinasPareizaVieta = 0;
 
     public void OnDrop(PointerEventData eventData)
     {
-		if (eventData.pointerDrag != null)
-		{
-			if (eventData.pointerDrag.tag.Equals(tag))
-			{
-				vietasZRot =
-				eventData.pointerDrag.
-				GetComponent<RectTransform>().transform.eulerAngles.z;
+        if (eventData.pointerDrag != null)
+        {
+            if (eventData.pointerDrag.tag.Equals(tag))
+            {
+                vietasZRot =
+                eventData.pointerDrag.
+                GetComponent<RectTransform>().transform.eulerAngles.z;
 
-				velkObjZRot =
-				GetComponent<RectTransform>().transform.eulerAngles.z;
+                velkObjZRot =
+                GetComponent<RectTransform>().transform.eulerAngles.z;
 
-				rotacijasStarpiba =
-				Mathf.Abs(vietasZRot - velkObjZRot);
+                rotacijasStarpiba =
+                Mathf.Abs(vietasZRot - velkObjZRot);
 
-				vietasIzm =
-				eventData.pointerDrag.
-				GetComponent<RectTransform>().localScale;
+                vietasIzm =
+                eventData.pointerDrag.
+                GetComponent<RectTransform>().localScale;
 
-				velkObjIzm =
-					GetComponent<RectTransform>().localScale;
+                velkObjIzm =
+                    GetComponent<RectTransform>().localScale;
 
-				xIzmStarpiba = Mathf.Abs(vietasIzm.x - velkObjIzm.x);
-				yIzmStarpiba = Mathf.Abs(vietasIzm.y - velkObjIzm.y);
+                xIzmStarpiba = Mathf.Abs(vietasIzm.x - velkObjIzm.x);
+                yIzmStarpiba = Mathf.Abs(vietasIzm.y - velkObjIzm.y);
 
-				Debug.Log("Objektu rotācijas starpība: " + rotacijasStarpiba
-					+ "\nPlatuma starpība: " + xIzmStarpiba +
-					"\nAugstuma starpība: " + yIzmStarpiba);
+                Debug.Log("Objektu rotācijas starpība: " + rotacijasStarpiba
+                    + "\nPlatuma starpība: " + xIzmStarpiba +
+                    "\nAugstuma starpība: " + yIzmStarpiba);
 
 
-				if ((rotacijasStarpiba <= 6 ||
-					(rotacijasStarpiba >= 354 && rotacijasStarpiba <= 360))
-					&& (xIzmStarpiba <= 0.1 && yIzmStarpiba <= 0.1))
-				{
-					Debug.Log("Nomests pareizajā vietā!");
+                if ((rotacijasStarpiba <= 6 ||
+                    (rotacijasStarpiba >= 354 && rotacijasStarpiba <= 360))
+                    && (xIzmStarpiba <= 0.1 && yIzmStarpiba <= 0.1))
+                {
+                    Debug.Log("Nomests pareizajā vietā!");
                     objektuSkripts.vaiIstajaVieta = true;
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition
-						= GetComponent<RectTransform>().anchoredPosition;
+                        = GetComponent<RectTransform>().anchoredPosition;
 
-					eventData.pointerDrag.GetComponent<RectTransform>().localRotation =
-						GetComponent<RectTransform>().localRotation;
+                    eventData.pointerDrag.GetComponent<RectTransform>().localRotation =
+                        GetComponent<RectTransform>().localRotation;
 
-					eventData.pointerDrag.GetComponent<RectTransform>().localScale =
-						GetComponent<RectTransform>().localScale;
+                    eventData.pointerDrag.GetComponent<RectTransform>().localScale =
+                        GetComponent<RectTransform>().localScale;
 
-					switch (eventData.pointerDrag.tag) {
-						case "atkritumi":
-							objektuSkripts.skanasAvots.PlayOneShot(
-								objektuSkripts.skanasKoAtskanot[1]);
+                    switch (eventData.pointerDrag.tag)
+                    {
+                        case "atkritumi":
+                            objektuSkripts.skanasAvots.PlayOneShot(
+                                objektuSkripts.skanasKoAtskanot[1]);
                             masinasPareizaVieta++;
                             Debug.Log("Mašinas pareiza vieta: " + masinasPareizaVieta);
                             break;
 
-						case "atrie":
-							objektuSkripts.skanasAvots.PlayOneShot(
-								objektuSkripts.skanasKoAtskanot[2]);
+                        case "atrie":
+                            objektuSkripts.skanasAvots.PlayOneShot(
+                                objektuSkripts.skanasKoAtskanot[2]);
                             masinasPareizaVieta++;
                             Debug.Log("Mašinas pareiza vieta: " + masinasPareizaVieta);
                             break;
 
-						case "buss":
-							objektuSkripts.skanasAvots.PlayOneShot(
-								objektuSkripts.skanasKoAtskanot[3]);
+                        case "buss":
+                            objektuSkripts.skanasAvots.PlayOneShot(
+                                objektuSkripts.skanasKoAtskanot[3]);
                             masinasPareizaVieta++;
                             Debug.Log("Mašinas pareiza vieta: " + masinasPareizaVieta);
                             break;
 
-						case "b2":
-							objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanasKoAtskanot[4]);
+                        case "b2":
+                            objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanasKoAtskanot[4]);
                             masinasPareizaVieta++;
                             Debug.Log("Mašinas pareiza vieta: " + masinasPareizaVieta);
                             break;
@@ -132,88 +134,105 @@ public class NomesanasVieta : MonoBehaviour,
                         case "ugunsdzeseji":
                             objektuSkripts.skanasAvots.PlayOneShot(objektuSkripts.skanasKoAtskanot[12]);
                             masinasPareizaVieta++;
-                            Debug.Log("Mašinas pareiza vieta: "+masinasPareizaVieta);
+                            Debug.Log("Mašinas pareiza vieta: " + masinasPareizaVieta);
                             break;
 
                         default:
-							Debug.Log("Tags nav definēts!");
-							break;
-					}
+                            Debug.Log("Tags nav definēts!");
+                            break;
+                    }
                     //Parbauda vai ir sasniegta uzvara
                     if (masinasPareizaVieta == 12)
                     {
                         Debug.Log("Uzvara!");
+
+                        // Find the GameObject with the UzvarasAttelosana component
+                        UzvarasAttelosana uzvarasAttelosana = FindObjectOfType<UzvarasAttelosana>();
+
+                        if (uzvarasAttelosana != null)
+                        {
+                            // Call the method on the existing instance of UzvarasAttelosana
+                            uzvarasAttelosana.AttelotUzvaru();
+                        }
+                        else
+                        {
+                            Debug.LogError("UzvarasAttelosana component not found!");
+                        }
+                    }
+
+                    //Ja tagi nesakrīt, tātad nepareizajā vietā
+                }
+                else
+                {
+                    objektuSkripts.vaiIstajaVieta = false;
+                    objektuSkripts.skanasAvots.PlayOneShot(
+                        objektuSkripts.skanasKoAtskanot[0]);
+
+                    switch (eventData.pointerDrag.tag)
+                    {
+                        case "atkritumi":
+                            objektuSkripts.atkritumuMasina.
+                            GetComponent<RectTransform>().localPosition =
+                            objektuSkripts.atkrMKoord;
+                            break;
+
+                        case "atrie":
+                            objektuSkripts.atraPalidziba.
+                            GetComponent<RectTransform>().localPosition =
+                            objektuSkripts.atrPKoord;
+                            break;
+
+                        case "buss":
+                            objektuSkripts.autobuss.
+                             GetComponent<RectTransform>().localPosition =
+                             objektuSkripts.bussKoord;
+                            break;
+
+                        case "b2":
+                            objektuSkripts.b2.GetComponent<RectTransform>().localPosition = objektuSkripts.b2Koord;
+                            break;
+
+                        case "cements":
+                            objektuSkripts.cements.GetComponent<RectTransform>().localPosition = objektuSkripts.cementsKoord;
+                            break;
+
+                        case "e46":
+                            objektuSkripts.e46.GetComponent<RectTransform>().localPosition = objektuSkripts.e46Koord;
+                            break;
+
+                        case "e61":
+                            objektuSkripts.e61.GetComponent<RectTransform>().localPosition = objektuSkripts.e61Koord;
+                            break;
+
+                        case "ekskavators":
+                            objektuSkripts.ekskavators.GetComponent<RectTransform>().localPosition = objektuSkripts.ekskavatorsKoord;
+                            break;
+
+                        case "policija":
+                            objektuSkripts.policija.GetComponent<RectTransform>().localPosition = objektuSkripts.policijasKoord;
+                            break;
+
+                        case "traktors":
+                            objektuSkripts.traktors.GetComponent<RectTransform>().localPosition = objektuSkripts.traktorsKoord;
+                            break;
+
+                        case "traktors2":
+                            objektuSkripts.traktors2.GetComponent<RectTransform>().localPosition = objektuSkripts.traktorsKoord2;
+                            break;
+
+                        case "ugunsdzeseji":
+                            objektuSkripts.ugunsdzeseji.GetComponent<RectTransform>().localPosition = objektuSkripts.ugunsdzesejiKoord;
+                            break;
+
+                        default:
+                            Debug.Log("Tags nav definēts!");
+                            break;
                     }
                 }
-
-				//Ja tagi nesakrīt, tātad nepareizajā vietā
-			} else	{
-				objektuSkripts.vaiIstajaVieta = false;
-				objektuSkripts.skanasAvots.PlayOneShot(
-					objektuSkripts.skanasKoAtskanot[0]);
-
-                switch (eventData.pointerDrag.tag){
-                    case "atkritumi":
-						objektuSkripts.atkritumuMasina.
-						GetComponent<RectTransform>().localPosition =
-						objektuSkripts.atkrMKoord;
-                        break;
-
-                    case "atrie":
-                        objektuSkripts.atraPalidziba.
-                        GetComponent<RectTransform>().localPosition =
-                        objektuSkripts.atrPKoord;
-                        break;
-                        
-                    case "buss":
-                        objektuSkripts.autobuss.
-                         GetComponent<RectTransform>().localPosition =
-                         objektuSkripts.bussKoord;
-                        break;
-
-					case "b2":
-						objektuSkripts.b2.GetComponent<RectTransform>().localPosition = objektuSkripts.b2Koord;
-						break;
-
-                    case "cements":
-                        objektuSkripts.cements.GetComponent<RectTransform>().localPosition = objektuSkripts.cementsKoord;
-                        break;
-
-                    case "e46":
-                        objektuSkripts.e46.GetComponent<RectTransform>().localPosition = objektuSkripts.e46Koord;
-                        break;
-
-                    case "e61":
-                        objektuSkripts.e61.GetComponent<RectTransform>().localPosition = objektuSkripts.e61Koord;
-                        break;
-
-                    case "ekskavators":
-                        objektuSkripts.ekskavators.GetComponent<RectTransform>().localPosition = objektuSkripts.ekskavatorsKoord;
-                        break;
-
-                    case "policija":
-                        objektuSkripts.policija.GetComponent<RectTransform>().localPosition = objektuSkripts.policijasKoord;
-                        break;
-
-                    case "traktors":
-                        objektuSkripts.traktors.GetComponent<RectTransform>().localPosition = objektuSkripts.traktorsKoord;
-                        break;
-
-                    case "traktors2":
-                        objektuSkripts.traktors2.GetComponent<RectTransform>().localPosition = objektuSkripts.traktorsKoord2;
-                        break;
-
-                    case "ugunsdzeseji":
-                        objektuSkripts.ugunsdzeseji.GetComponent<RectTransform>().localPosition = objektuSkripts.ugunsdzesejiKoord;
-                        break;
-
-                    default:
-                        Debug.Log("Tags nav definēts!");
-                        break;
-                }
             }
-		}
-		
-	}
 
+        }
+    }
 }
+
+
--- Assets/Skripti/UzvarasAttelosana.cs
+++ Assets/Skripti/UzvarasAttelosana.cs
@@ -37,7 +37,46 @@ public class UzvarasAttelosana : MonoBehaviour
     }
     public void AttelotUzvaru()
     {
+        // Aprēķina pagājušo laiku
+        float elapsedTime = Time.time - startTime;
 
+        // Iestata uzvarasPanelis objektu redzamu
+        uzvarasPanelis.transform.localScale = Vector3.one;
+
+        // Pārbauda pagājušo laiku un iestata redzamo zvaigžņu skaitu atbilstoši noteiktajiem nosacījumiem
+        if (elapsedTime < 120f)
+        {
+            // Parāda 3 zvaigznes
+            zvaignze1.transform.localScale = Vector3.one;
+            zvaignze2.transform.localScale = Vector3.one;
+            zvaignze3.transform.localScale = Vector3.one;
+            Debug.Log("Elapsed Time: " + elapsedTime.ToString("F2") + "- 3 zvaigznes");
+
+        }
+        else if (elapsedTime >= 120f && elapsedTime < 240f)
+        {
+            // Parāda 2 zvaigznes
+            zvaignze1.transform.localScale = Vector3.one;
+            zvaignze2.transform.localScale = Vector3.one;
+            zvaignze3.transform.localScale = Vector3.zero;
+            Debug.Log("Elapsed Time: " + elapsedTime.ToString("F2") + " - 2 zvaigznes");
+        }
+        else if (elapsedTime >= 240f && elapsedTime < 360f)
+        {
+            // Parāda 1 zvaigzni
+            zvaignze1.transform.localScale = Vector3.one;
+            zvaignze2.transform.localScale = Vector3.zero;
+            zvaignze3.transform.localScale = Vector3.zero;
+            Debug.Log("Elapsed Time: " + elapsedTime.ToString("F2") + " - 1 zvaigzne");
+        }
+        else
+        {
+            // Nav zvaigžņu
+            zvaignze1.transform.localScale = Vector3.zero;
+            zvaignze2.transform.localScale = Vector3.zero;
+            zvaignze3.transform.localScale = Vector3.zero;
+            Debug.Log("Elapsed Time: " + elapsedTime.ToString("F2") + "- 0 zvaigznes");
+        }
     }
 
 
